01cc7b9c5eea76be8fb7ae6c22c87ad6a7cd9420
Jimin Lee <jiminlee102@gmail.com>
2025-02-23 00:52:37 -0800

    fixed deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	optimizeDeps: {
+		esbuildOptions: {
+			target: 'esnext'
+		}
+	},
+	build: {
+		target: 'esnext'
+	},
 	plugins: [sveltekit()]
 });
