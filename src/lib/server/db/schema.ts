9e1ae78cd13bb2e533ae1fe53e575f9d2a39509d
Jimin Lee <jiminlee102@gmail.com>
2025-04-14 22:19:47 -0700

    Initial commit

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,22 @@
+import { pgTable, integer, varchar, boolean } from 'drizzle-orm/pg-core';
+
+export const singleLockers = pgTable('singleLockers', {
+	id: integer().primaryKey(),
+	user_id: integer(),
+	name: varchar(),
+	grade: integer(),
+	student_id: integer(),
+	available: boolean()
+});
+
+export const partnerLockers = pgTable('partnerLockers', {
+	id: integer().primaryKey(),
+	user_id: integer(),
+	primary_name: varchar(),
+	primary_grade: integer(),
+	primary_student_id: integer(),
+	secondary_name: varchar(),
+	secondary_grade: integer(),
+	secondary_student_id: integer(),
+	available: boolean()
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<main class="flex h-lvh flex-col items-center justify-center gap-2">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Tino Lockers</h1>
+	<p class="leading-7">One simple place to request a new locker!</p>
+	<Button class="mt-4">Sign in with Google</Button>
+</main>
