252f8c8217de3fd9eb527e3cca1357e1311d60da
Jimin Lee <jiminlee102@gmail.com>
2025-04-17 18:45:00 -0700

    admin dashboard ui setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { toast } from 'svelte-sonner';
-	import { CirclePlus, KeyRound, LogIn, LogOut } from 'lucide-svelte';
+	import { CirclePlus, KeyRound, LayoutDashboard, LogIn, LogOut } from 'lucide-svelte';
 	import {
 		partnerLockerRequestFormSchema,
 		singleLockerRequestFormSchema,
@@ -16,6 +16,7 @@
 	import { zodClient } from 'sveltekit-superforms/adapters';
 	import { authClient } from '$lib/auth/auth-client';
 	import ScrollArea from '$lib/components/ui/scroll-area/scroll-area.svelte';
+	import { Badge } from '$lib/components/ui/badge';
 
 	const session = authClient.useSession();
 
@@ -57,16 +58,29 @@
 
 <main class="flex h-lvh select-none flex-col items-center justify-center gap-2">
 	{#if $session.data}
-		<p class="text-muted-foreground">
-			Logged in as <strong>{$session.data?.user.name}</strong>
-		</p>
+		<div class="flex gap-1">
+			<p class="text-muted-foreground">
+				Logged in as <strong>{$session.data.user.name}</strong>
+			</p>
+			{#if $session.data.user.role === 'admin'}
+				<Badge variant="secondary">Admin</Badge>
+			{/if}
+		</div>
 	{/if}
-	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Tino Lockers</h1>
-	<p class="mb-4 leading-7">One simple place to request a new locker!</p>
+	<h1 class="scroll-m-20 text-5xl font-extrabold tracking-tight">Tino Lockers</h1>
+	<p class="mb-4 leading-7">
+		{$session.data.user.role === 'user'
+			? 'One simple place to request a new locker!'
+			: 'One simple place to manage your lockers!'}
+	</p>
 	{#if $session.data}
 		<div class="flex gap-2">
-			<Button href="#request"><CirclePlus /> Request a locker</Button>
-			<Button href="/my-locker" variant="secondary"><KeyRound /> My locker</Button>
+			{#if $session.data.user.role === 'user'}
+				<Button href="#request"><CirclePlus /> Request a locker</Button>
+				<Button href="/my-locker" variant="secondary"><KeyRound /> My locker</Button>
+			{:else if $session.data.user.role === 'admin'}
+				<Button href="/admin"><LayoutDashboard /> Dashboard</Button>
+			{/if}
 			<Button
 				variant="outline"
 				onclick={async () => {
@@ -85,171 +99,175 @@
 	{/if}
 </main>
 {#if $session.data}
-	<div id="request" class="flex h-lvh flex-col items-center pt-20">
-		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Request a locker</h1>
-		<Tabs.Root value="single" class="mt-10 w-[400px]">
-			<Tabs.List class="grid w-full grid-cols-2">
-				<Tabs.Trigger value="single">Single</Tabs.Trigger>
-				<Tabs.Trigger value="partner">Partner</Tabs.Trigger>
-			</Tabs.List>
-			<Tabs.Content value="single">
-				<form method="POST" use:singleEnhance class="mt-4 flex flex-col gap-y-4">
-					<Form.Field form={singleForm} name="name">
-						<Form.Control>
-							<Form.Label>Full Name</Form.Label>
-							<Input bind:value={$singleFormData.name} />
-						</Form.Control>
-						<Form.FieldErrors />
-					</Form.Field>
-					<Form.Fieldset form={singleForm} name="grade">
-						<Form.Legend>Grade</Form.Legend>
-						<RadioGroup.Root bind:value={$singleFormData.grade} name="grade" class="flex gap-x-4">
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="9" />
-									<Form.Label class="font-normal">9</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="10" />
-									<Form.Label class="font-normal">10</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="11" />
-									<Form.Label class="font-normal">11</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="12" />
-									<Form.Label class="font-normal">12</Form.Label>
-								</Form.Control>
-							</div>
-						</RadioGroup.Root>
-						<Form.FieldErrors />
-					</Form.Fieldset>
-					<Form.Field form={singleForm} name="student_id">
-						<Form.Control>
-							<Form.Label>Student ID</Form.Label>
-							<Input bind:value={$singleFormData.student_id} />
-						</Form.Control>
-						<Form.FieldErrors />
-					</Form.Field>
-					<Form.Field form={singleForm} name="requested_locker_id">
-						<Form.Control>
-							<Form.Label>Choose a locker #</Form.Label>
-							<ScrollArea class="h-80 rounded-md border border-input"></ScrollArea>
-						</Form.Control>
-						<Form.FieldErrors />
-					</Form.Field>
-					<Form.Button class="ml-auto">Submit</Form.Button>
-				</form>
-			</Tabs.Content>
-			<Tabs.Content value="partner">
-				<form method="POST" use:partnerEnhance class="mt-4 flex flex-col gap-y-4">
-					<Form.Field form={partnerForm} name="primary_name">
-						<Form.Control>
-							<Form.Label>Your Full Name</Form.Label>
-							<Input bind:value={$partnerFormData.primary_name} />
-						</Form.Control>
-						<Form.FieldErrors />
-					</Form.Field>
-					<Form.Fieldset form={partnerForm} name="primary_grade">
-						<Form.Legend>Your Grade</Form.Legend>
-						<RadioGroup.Root
-							bind:value={$partnerFormData.primary_grade}
-							name="primary_grade"
-							class="flex gap-x-4"
-						>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="9" />
-									<Form.Label class="font-normal">9</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="10" />
-									<Form.Label class="font-normal">10</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="11" />
-									<Form.Label class="font-normal">11</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="12" />
-									<Form.Label class="font-normal">12</Form.Label>
-								</Form.Control>
-							</div>
-						</RadioGroup.Root>
-						<Form.FieldErrors />
-					</Form.Fieldset>
-					<Form.Field form={partnerForm} name="primary_student_id">
-						<Form.Control>
-							<Form.Label>Your Student ID</Form.Label>
-							<Input bind:value={$partnerFormData.primary_student_id} />
-						</Form.Control>
-						<Form.FieldErrors />
-					</Form.Field>
-					<hr />
-					<Form.Field form={partnerForm} name="secondary_name">
-						<Form.Control>
-							<Form.Label>Partner Full Name</Form.Label>
-							<Input bind:value={$partnerFormData.secondary_name} />
-						</Form.Control>
-						<Form.FieldErrors />
-					</Form.Field>
-					<Form.Fieldset form={partnerForm} name="secondary_grade">
-						<Form.Legend>Partner Grade</Form.Legend>
-						<RadioGroup.Root
-							bind:value={$partnerFormData.secondary_grade}
-							name="secondary_grade"
-							class="flex gap-x-4"
-						>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="9" />
-									<Form.Label class="font-normal">9</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="10" />
-									<Form.Label class="font-normal">10</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="11" />
-									<Form.Label class="font-normal">11</Form.Label>
-								</Form.Control>
-							</div>
-							<div class="flex items-center space-x-2 space-y-0">
-								<Form.Control>
-									<RadioGroup.Item value="12" />
-									<Form.Label class="font-normal">12</Form.Label>
-								</Form.Control>
-							</div>
-						</RadioGroup.Root>
-						<Form.FieldErrors />
-					</Form.Fieldset>
-					<Form.Field form={partnerForm} name="secondary_student_id">
-						<Form.Control>
-							<Form.Label>Partner Student ID</Form.Label>
-							<Input bind:value={$partnerFormData.secondary_student_id} />
-						</Form.Control>
-						<Form.FieldErrors />
-					</Form.Field>
-					<Form.Button class="ml-auto">Submit</Form.Button>
-				</form>
-			</Tabs.Content>
-		</Tabs.Root>
-	</div>
+	{#if $session.data.user.role === 'user'}
+		<div id="request" class="flex h-lvh flex-col items-center pt-20">
+			<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+				Request a locker
+			</h1>
+			<Tabs.Root value="single" class="mt-10 w-[400px]">
+				<Tabs.List class="grid w-full grid-cols-2">
+					<Tabs.Trigger value="single">Single</Tabs.Trigger>
+					<Tabs.Trigger value="partner">Partner</Tabs.Trigger>
+				</Tabs.List>
+				<Tabs.Content value="single">
+					<form method="POST" use:singleEnhance class="mt-4 flex flex-col gap-y-4">
+						<Form.Field form={singleForm} name="name">
+							<Form.Control>
+								<Form.Label>Full Name</Form.Label>
+								<Input bind:value={$singleFormData.name} />
+							</Form.Control>
+							<Form.FieldErrors />
+						</Form.Field>
+						<Form.Fieldset form={singleForm} name="grade">
+							<Form.Legend>Grade</Form.Legend>
+							<RadioGroup.Root bind:value={$singleFormData.grade} name="grade" class="flex gap-x-4">
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="9" />
+										<Form.Label class="font-normal">9</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="10" />
+										<Form.Label class="font-normal">10</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="11" />
+										<Form.Label class="font-normal">11</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="12" />
+										<Form.Label class="font-normal">12</Form.Label>
+									</Form.Control>
+								</div>
+							</RadioGroup.Root>
+							<Form.FieldErrors />
+						</Form.Fieldset>
+						<Form.Field form={singleForm} name="student_id">
+							<Form.Control>
+								<Form.Label>Student ID</Form.Label>
+								<Input bind:value={$singleFormData.student_id} />
+							</Form.Control>
+							<Form.FieldErrors />
+						</Form.Field>
+						<Form.Field form={singleForm} name="requested_locker_id">
+							<Form.Control>
+								<Form.Label>Choose a locker #</Form.Label>
+								<ScrollArea class="h-80 rounded-md border border-input"></ScrollArea>
+							</Form.Control>
+							<Form.FieldErrors />
+						</Form.Field>
+						<Form.Button class="ml-auto">Submit</Form.Button>
+					</form>
+				</Tabs.Content>
+				<Tabs.Content value="partner">
+					<form method="POST" use:partnerEnhance class="mt-4 flex flex-col gap-y-4">
+						<Form.Field form={partnerForm} name="primary_name">
+							<Form.Control>
+								<Form.Label>Your Full Name</Form.Label>
+								<Input bind:value={$partnerFormData.primary_name} />
+							</Form.Control>
+							<Form.FieldErrors />
+						</Form.Field>
+						<Form.Fieldset form={partnerForm} name="primary_grade">
+							<Form.Legend>Your Grade</Form.Legend>
+							<RadioGroup.Root
+								bind:value={$partnerFormData.primary_grade}
+								name="primary_grade"
+								class="flex gap-x-4"
+							>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="9" />
+										<Form.Label class="font-normal">9</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="10" />
+										<Form.Label class="font-normal">10</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="11" />
+										<Form.Label class="font-normal">11</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="12" />
+										<Form.Label class="font-normal">12</Form.Label>
+									</Form.Control>
+								</div>
+							</RadioGroup.Root>
+							<Form.FieldErrors />
+						</Form.Fieldset>
+						<Form.Field form={partnerForm} name="primary_student_id">
+							<Form.Control>
+								<Form.Label>Your Student ID</Form.Label>
+								<Input bind:value={$partnerFormData.primary_student_id} />
+							</Form.Control>
+							<Form.FieldErrors />
+						</Form.Field>
+						<hr />
+						<Form.Field form={partnerForm} name="secondary_name">
+							<Form.Control>
+								<Form.Label>Partner Full Name</Form.Label>
+								<Input bind:value={$partnerFormData.secondary_name} />
+							</Form.Control>
+							<Form.FieldErrors />
+						</Form.Field>
+						<Form.Fieldset form={partnerForm} name="secondary_grade">
+							<Form.Legend>Partner Grade</Form.Legend>
+							<RadioGroup.Root
+								bind:value={$partnerFormData.secondary_grade}
+								name="secondary_grade"
+								class="flex gap-x-4"
+							>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="9" />
+										<Form.Label class="font-normal">9</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="10" />
+										<Form.Label class="font-normal">10</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="11" />
+										<Form.Label class="font-normal">11</Form.Label>
+									</Form.Control>
+								</div>
+								<div class="flex items-center space-x-2 space-y-0">
+									<Form.Control>
+										<RadioGroup.Item value="12" />
+										<Form.Label class="font-normal">12</Form.Label>
+									</Form.Control>
+								</div>
+							</RadioGroup.Root>
+							<Form.FieldErrors />
+						</Form.Fieldset>
+						<Form.Field form={partnerForm} name="secondary_student_id">
+							<Form.Control>
+								<Form.Label>Partner Student ID</Form.Label>
+								<Input bind:value={$partnerFormData.secondary_student_id} />
+							</Form.Control>
+							<Form.FieldErrors />
+						</Form.Field>
+						<Form.Button class="ml-auto">Submit</Form.Button>
+					</form>
+				</Tabs.Content>
+			</Tabs.Root>
+		</div>
+	{/if}
 {/if}
