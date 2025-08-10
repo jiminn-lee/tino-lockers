35ad7dfc9d17ea15f89c71f7062c02d58677a053
Jimin Lee <jiminlee102@gmail.com>
2025-08-10 12:40:19 -0700

    toggle switch for accepting requests

--- src/routes/admin/requests/+page.svelte
+++ src/routes/admin/requests/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import BackButton from '$lib/components/BackButton.svelte';
+	import * as Card from '$lib/components/ui/card/index.js';
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
+	import { Switch } from '$lib/components/ui/switch/index.js';
 	import { singleRequestColumns } from '$lib/components/admin/requests/single/columns.js';
 	import DataTable from '$lib/components/admin/requests/data-table.svelte';
 	import { partnerRequestColumns } from '$lib/components/admin/requests/partner/columns.js';
@@ -13,7 +15,13 @@
 	<BackButton class="ml-10" />
 	<div class="flex flex-col items-center">
 		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Manage Requests</h1>
-		<Tabs.Root value="single" class="mt-10">
+		<div
+			class="bg-card text-card-foreground mt-10 mb-5 flex w-[500px] flex-row items-center justify-between rounded-xl border p-4 shadow-sm"
+		>
+			<h1 class="leading-none font-semibold">Accepting requests</h1>
+			<Switch />
+		</div>
+		<Tabs.Root value="single">
 			<Tabs.List class="grid w-full grid-cols-2">
 				<Tabs.Trigger value="single"><User weight="bold" class="mr-2" /> Single</Tabs.Trigger>
 				<Tabs.Trigger value="partner"><Users weight="bold" class="mr-2" />Partner</Tabs.Trigger>
